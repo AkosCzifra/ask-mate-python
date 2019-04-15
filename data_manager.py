e39208dabbb89bb30f08bc583b27f7286b110808
Mark Ungvari <ungvarimark@gmail.com>
2019-04-15 14:40:47 +0200

    Rearranged the functions from data_manager.py to connection.py and fixed their return values

--- data_manager.py
+++ data_manager.py
@@ -1,31 +1,10 @@
-import csv
-import os
 import string
 import random
-
-
-QUESTION_CSV = 'question.csv'
-ANSWER_CSV = 'answer.csv'
-QUESTION_CSV_PATH = os.getenv('QUESTION_CSV_PATH') if 'QUESTION_CSV_PATH' in os.environ else QUESTION_CSV
-ANSWER_CSV_PATH = os.getenv('ANSWER_CSV_PATH') if 'ANSWER_CSV_PATH' in os.environ else ANSWER_CSV
-QUESTION_HEADER = ["id", "submission_time", "view_number", "vote_number", "title", "message", "image"]
-ANSWER_HEADER = ["id", "submission_time", "vote_number", "question_id", "message", "image"]
-
-
-def get_csv_question_data(one_question_id=None):
-    user_questions = []
-    with open (QUESTION_CSV_PATH, encoding='utf-8') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            user_question = dict(row)
-            if one_question_id is not None and one_question_id == user_question['id']:
-                return user_question
-            user_questions.append(user_question)
-    return user_questions
+import connection
 
 
 def get_all_questions():
-    all_questions = get_csv_question_data()
+    all_questions = connection.get_csv_question_data()
     return all_questions
 
 
