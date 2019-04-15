d95498905000f63713a89aa0b6d44f18272cca17
Mark Ungvari <ungvarimark@gmail.com>
2019-04-15 15:27:57 +0200

    Added the multiline string support function to the data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -3,8 +3,14 @@ import random
 import connection
 
 
-def get_all_questions():
+def get_all_questions(convert_linebreak=False):
     all_questions = connection.get_csv_question_data()
+
+    if convert_linebreak:
+        for question in all_questions:
+            question["title"] = convert_linebreak_to_br(question["title"])
+            question["message"] = convert_linebreak_to_br(question["message"])
+
     return all_questions
 
 
@@ -13,6 +19,9 @@ def generate_id():
     id_ = "".join(rand_letters)
     return id_
 
+
+def convert_linebreak_to_br(original_str):
+    return '<br>'.join(original_str.split('\n'))
 
 
 
