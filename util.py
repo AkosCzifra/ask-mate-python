dd860d6fbf3e7fae336363bd015ab028f578355c
Akos Czifra <akosczifraa@gmail.com>
2019-04-16 17:23:34 +0200

    added convert to util

--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
 
-def unix_to_date(timestamp):
+def unix_to_date(timestamp, timezone=7200):
+    timestamp += timezone
     return datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
